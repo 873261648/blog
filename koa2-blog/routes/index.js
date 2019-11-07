54bdedd725a91cb48f71254eaaa89931927724ee
small white <873261648@qq.com>
2019-11-07 19:31:40 +0800

    express原理，koa2初始化
    express原理，koa2初始化

---
+++ koa2-blog/routes/index.js
@@ -0,0 +1,27 @@
+const router = require('koa-router')();
+const path = require('path');
+const fs = require('fs');
+
+
+router.get('/', async (ctx, next) => {
+  let fullPath = path.join(__dirname, '../', 'views', 'index.html');
+  ctx.response.type = 'html';
+  ctx.body = fs.createReadStream(fullPath);
+
+
+  // await ctx.render('index', {
+  //   title: 'Hello Koa 2!'
+  // })
+});
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+});
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+});
+
+module.exports = router;
