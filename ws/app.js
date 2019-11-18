c94825d3fde71fc8367b1979b6244f4aa1ed0413
small white <873261648@qq.com>
2019-11-18 21:50:31 +0800

    websocket
    websocket

--- ws/app.js
+++ ws/app.js
@@ -5,22 +5,34 @@ function callback(req, res) {
     res.setHeader('ContentType', "text");
     res.send("hello")
 }
+
 // http和websocket统一端口
 const app = http.createServer(callback)
 
 const wsServer = new WebSocket.Server({
     server: app
-})
-
-wsServer.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        wss.client
-
-        console.log('received: %s', message);
-    });
-    ws.send('连接成功');
 });
 
-app.listen(3000,()=>{
+// 有新用户接入
+wsServer.on('connection', function connection(ws) {
+    ws.data = {
+        'userName': 1,
+        'headerImg': 2
+    };
+
+
+    ws.on('message', function (message) {
+        wsServer.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send('有人发了一条新消息');
+                client.send(JSON.stringify(ws.data));
+            }
+        });
+        console.log('received: %s', message);
+    });
+    ws.send(JSON.stringify(ws.data));
+});
+
+app.listen(3000, () => {
     console.log('server at http://localhost:3000')
 })
